6fd13366853488cb73cc4c6cc3f7e2f67389eef1
goncalhurtado <goncalhurtado@gmail.com>
2024-03-11 00:42:31 -0300

    fixed bug

--- config/axiosInstance.js
+++ config/axiosInstance.js
@@ -4,5 +4,5 @@ import { API_URL } from "@env";
 const url = API_URL;
 export const axiosInstance = axios.create({
   baseURL: "http://localhost:8080/api/v1",
-  // baseURL: url,
+  baseURL: `${url}/api/v1`,
 });
